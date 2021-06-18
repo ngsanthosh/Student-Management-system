2e7948811f92dbe079e2054904e074f1f29bbe0c
ngsanthosh <ngsanthosh657@gmail.com>
2021-06-18 21:52:34 +0530

    Add CRUD Operations

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from api import views
 urlpatterns = [
-    
+    path('students/', views.StudentListView.as_view()),
+    path('student/<int:pk>', views.StudentDetailView.as_view()),
+    path('delete/<int:pk>', views.StudentDeleteView.as_view())
 ]
